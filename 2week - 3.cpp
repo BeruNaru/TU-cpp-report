3935fc0bb63e266a8509b35699ddb89184635180
Beru <91617802+BeruNaru@users.noreply.github.com>
2023-03-16 22:50:04 +0900

    Create 2week - 3.cpp

---
+++ 2week - 3.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ 
+    cout <<  "학과 :"<< "정보보호학과 " << endl;
+    cout << "성명: " << "송기원 " << endl;
+    cout << "학번: " << "22****12" << endl;
+    cout << "나이: " << "21"<< endl;
+    cout <<  "성별:" << "남자 "<< endl;
+    return 0;
+}
